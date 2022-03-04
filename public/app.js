4d69416817ce70e6c1ae7048db8411c4be247237
nicodwi_k <nicodwika@gmail.com>
2022-03-04 13:32:04 +0700

    update

--- public/app.js
+++ public/app.js
@@ -10,206 +10,223 @@ var createRoomBtn = document.getElementById("room_add_icon_holder");
 var chatDisplay = document.getElementById("chat");
 
 var currentRoom = {
-    id : 'global',
-    name : 'Global',
-    avatar : '',
-    type : '',
-    isGroup : false ,
+    id : 'global', // wajib, id room
+    name : 'Global', // wajib, nama room
+    avatar : '', // optional, sesuaikan kebutuhan UI (nama object FE & Mobile harus sama dan ada, agar tdk undifined variable)
+    type : '', // optional, sesuaikan kebutuhan UI (nama object FE & Mobile harus sama dan ada, agar tdk undifined variable)
+    isGroup : false , // optional, sesuaikan kebutuhan UI (nama object FE & Mobile harus sama dan ada, agar tdk undifined variable)
 };
 
 var user = {
-  token : "",
-  name : "",
-  id : "",
-  avatar : "",
+    token : "", // optional, hanya untuk send JWT auth ke API
+    name : "", // wajib
+    id : "", // wajib
+    avatar : "", // optional, sesuaikan kebutuhan UI (nama object FE & Mobile harus sama dan ada, agar tdk undifined variable)
 };
-let rooms = [];
-let targets = [];
-// let api = 'https://dev-api.ggl.life';
-let api = 'http://api.ggl';
+let rooms = []; //hanya untuk penampung list room
+let targets = []; //hanya untuk penampung list UUID person yang dikirim notifnya
+const api = 'https://dev-api.ggl.life';
+// const api = 'http://api.ggl';
 
 email = prompt("Email");
 password = prompt("password");
 login(email, password);
-// Prompt for username on connecting to server
+
+//on connecting to server
 socket.on("connect", function () {
-  console.log("Connect to socket");
+    console.log("Connect to socket");
 });
 
 // Send message on button click
 sendMessageBtn.addEventListener("click", function () {
-  data = {
-      from : {
-          id : user.id,
-          name : user.name,
-          avatar : user.avatar,
-      },
-      room : currentRoom,
-      message : message.value,
-      type : 'string',
-      opposites : targets
-  }
-  socket.emit("sendMessage", JSON.stringify(data)); // socket
-  createMessage(message.value); // lumen
-  message.value = "";
+    data = {
+        message : message.value, // wajib, untuk data pesan
+        opposites : targets, // wajib, untuk target user yang dikirim notifikasi pesan masuk
+        // ambil dari list members di room yg bersangkutam
+        person : {
+            id : user.id,
+            name : user.name,
+            avatar : user.avatar,
+        }, // optional, sesuaikan kebutuhan UI (nama object FE & Mobile harus sama dan ada, agar tdk undifined variable)
+        type : 'text', // optional, sesuaikan kebutuhan UI (nama object FE & Mobile harus sama dan ada, agar tdk undifined variable)
+        room : currentRoom, // optional, sesuaikan kebutuhan UI (nama object FE & Mobile harus sama dan ada, agar tdk undifined variable)
+        parent : {
+            id : '',
+            message : '',
+            type : '',
+            person : {
+                id : '',
+                name : '',
+                avatar : '',
+            }
+        } // optional, sesuaikan kebutuhan UI (nama object FE & Mobile harus sama dan ada, agar tdk undifined variable)
+    }
+    socket.emit("sendMessage", JSON.stringify(data)); // socket
+    createMessage(message.value); // lumen
+    message.value = "";
 });
 
 // Send message on enter key press
 message.addEventListener("keyup", function (event) {
-  if (event.key === "Enter") {
-    sendMessageBtn.click();
-  }
+    if (event.key === "Enter") {
+        sendMessageBtn.click();
+    }
 });
 
-// Create new room on button click
-createRoomBtn.addEventListener("click", function () {
-  // socket.emit("createRoom", prompt("Enter new room: "));
-  let roomName = roomInput.value.trim();
-  if (roomName !== "") {
-    socket.emit("createRoom", roomName);
-    roomInput.value = "";
-  }
+// jika ada pesan baru
+socket.on("updateChat", function (event, data) {
+    data = JSON.parse(data);
+    console.log(data);
+    if (event === "INFO") {
+        console.log("Displaying announcement");
+        chatDisplay.innerHTML += `<div class="announcement"><span>${data.message}</span></div>`;
+    } else if(event === "NEW") {
+        className = data.person.id === user.id ? "me" : "";
+        chatDisplay.innerHTML += `<div class="message_holder ${className}">
+                                    <div class="pic"></div>
+                                    <div class="message_box">
+                                    <div id="message" class="message">
+                                        <span class="message_name">${data.person.name}</span>
+                                        <span class="message_text">${data.message}</span>
+                                    </div>
+                                    </div>
+                                </div>`;
+    }
+
+    chatDisplay.scrollTop = chatDisplay.scrollHeight;
 });
 
-socket.on("updateChat", function (userevent, data) {
-  if (userevent === "INFO") {
-    console.log("Displaying announcement");
-    chatDisplay.innerHTML += `<div class="announcement"><span>${data}</span></div>`;
-  } else {
-    console.log("Displaying user message");
-    chatDisplay.innerHTML += `<div class="message_holder ${
-      userevent.id === user.id ? "me" : ""
-    }">
-                                <div class="pic"></div>
-                                <div class="message_box">
-                                  <div id="message" class="message">
-                                    <span class="message_name">${userevent.name}</span>
-                                    <span class="message_text">${data}</span>
-                                  </div>
-                                </div>
-                              </div>`;
-  }
-
-  chatDisplay.scrollTop = chatDisplay.scrollHeight;
+// jika ada error di server
+socket.on("onError", function (event, data) {
+    console.log(data);
+    data = JSON.parse(data);
+    $('.toast strong').html(`WebSocket Server Error`);
+    $('.toast .toast-body').html(data.message);
+    $('.toast').toast('show');
 });
 
-socket.on(`getNotif`, function (userevent, data) {
+// ada event notif dari server
+socket.on(`getNotif`, function (event, data) {
     data = JSON.parse(data);
     if (data.room.isGroup) {
         $('.toast strong').html(`Pesan baru dari ${data.room.name}`);
-        $('.toast .toast-body').html(`${data.from.name} : ${data.message}`);
+        $('.toast .toast-body').html(`${data.person.name} : ${data.message}`);
     }else{
-        $('.toast strong').html(`Pesan baru dari ${data.from.name}`);
+        $('.toast strong').html(`Pesan baru dari ${data.person.name}`);
         $('.toast .toast-body').html(data.message);
     }
     $('.toast').toast('show');
 });
 
+//list users connected in server
 socket.on("updateUsers", function (users) {
-  userlist.innerHTML = "";
-  console.log("user returned from server", users);
-  for (var user in users) {
-    userlist.innerHTML += `<div class="user_card">
-                              <div class="pic"></div>
-                              <span>${users[user].name}</span>
-                            </div>`;
-  }
+    userlist.innerHTML = "";
+    console.log("user returned from server", users);
+    for (var user in users) {
+        userlist.innerHTML += `<div class="user_card">
+                                <div class="pic"></div>
+                                <span>${users[user].name}</span>
+                                </div>`;
+    }
 });
 
 
 socket.on("connect_error", () => {
-  setTimeout(() => {
-    socket.connect();
-  }, 1000);
+    setTimeout(() => {
+        socket.connect();
+    }, 1000);
 });
 
 socket.on("disconnect", (reason) => {
-  console.log(reason);
+    $('.toast strong').html(`WebSocket Disconnected`);
+    $('.toast .toast-body').html(reason);
+    $('.toast').toast('show');
 });
 
 function changeRoom(element) {
-  data = {
-    id : $(element).data('id'),
-    name : $(element).data('name'),
-    avatar : $(element).data('avatar'),
-    type : $(element).data('type'),
-    isGroup : $(element).data('isgroup'),
-  }
-  if (data.id != currentRoom.id) {
-    socket.emit("updateRooms", data);
-    currentRoom = data;
-    chatDisplay.innerHTML = "";
-    listMessage();
-    rooms.forEach( element => {
-        if (element.id == data.id) {
-            console.log(element);
-            targets = element.members.map( e => e.id);
-        }
-    });
-  }
+    data = {
+        id : $(element).data('id'),
+        name : $(element).data('name'),
+        avatar : $(element).data('avatar'),
+        type : $(element).data('type'),
+        isGroup : $(element).data('isgroup'),
+    }
+    if (data.id != currentRoom.id) {
+        socket.emit("updateRooms", data);
+        currentRoom = data;
+        chatDisplay.innerHTML = "";
+        listMessage();
+        rooms.forEach( element => {
+            if (element.id == data.id) {
+                console.log(element);
+                targets = element.members.map( e => e.id);
+            }
+        });
+    }
 }
 
 
 function listRoom(){
     //get list room by JWT
-  var myHeaders = new Headers();
-  myHeaders.append("Authorization", `Bearer ${user.token}`);
-  var requestOptions = {
-    method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow'
-  };
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", `Bearer ${user.token}`);
+    var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
 
-  fetch(`${api}/v1/chatting/list-room`, requestOptions)
-    .then(response => response.json())
-    .then(result => {
-      roomlist.innerHTML = "";
-      rooms = result.msgServer.data;
-      rooms.forEach(element => {
-        roomlist.innerHTML += `<div class="room_card" id="${element.id}"
-                                    onclick="changeRoom(this)"
-                                    data-id="${element.id}"
-                                    data-name="${element.name}"
-                                    data-avatar="${element.avatar}"
-                                    data-type="${element.type}"
-                                    data-isGroup="${element.is_group}"
-                                    >
-                                    <div class="room_item_content">
-                                        <div class="pic"></div>
-                                        <div class="roomInfo">
-                                        <span class="room_name">#${element.name}</span>
-                                        <span class="room_author">${element.last_message.person.name} : ${element.last_message.message}</span>
+    fetch(`${api}/v1/chatting/list-room`, requestOptions)
+        .then(response => response.json())
+        .then(result => {
+        roomlist.innerHTML = "";
+        rooms = result.msgServer.data;
+        rooms.forEach(element => {
+            last_message = element.last_message ? `${element.last_message.person.name} : ${element.last_message.message}` : '';
+            roomlist.innerHTML += `<div class="room_card" id="${element.id}"
+                                        onclick="changeRoom(this)"
+                                        data-id="${element.id}"
+                                        data-name="${element.name}"
+                                        data-avatar="${element.avatar}"
+                                        data-type="${element.type}"
+                                        data-isGroup="${element.is_group}"
+                                        >
+                                        <div class="room_item_content">
+                                            <div class="pic"></div>
+                                            <div class="roomInfo">
+                                            <span class="room_name">#${element.name}</span>
+                                            <span class="room_author">${last_message}</span>
+                                            </div>
                                         </div>
-                                    </div>
-                                </div>`;
-      });
+                                    </div>`;
+        });
 
-    })
-    .catch(error => console.log('error', error));
+        })
+        .catch(error => console.log('error', error));
 
 }
 
 function createMessage(message){
-  var myHeaders = new Headers();
-  myHeaders.append("Authorization", `Bearer ${user.token}`);
-  myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", `Bearer ${user.token}`);
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
 
-  var urlencoded = new URLSearchParams();
-  urlencoded.append("status", "created");
-  urlencoded.append("room", currentRoom.id);
-  urlencoded.append("message", message);
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("status", "created");
+    urlencoded.append("room", currentRoom.id);
+    urlencoded.append("message", message);
 
-  var requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: urlencoded,
-    redirect: 'follow'
-  };
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: urlencoded,
+        redirect: 'follow'
+    };
 
-  fetch(`${api}/v1/chatting/create-message`, requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+    fetch(`${api}/v1/chatting/create-message`, requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
 }
 
 function listMessage(){
@@ -243,28 +260,28 @@ function listMessage(){
 }
 
 function login(email, password){
-  var formdata = new FormData();
-  formdata.append("username", email);
-  formdata.append("password", password);
+    var formdata = new FormData();
+    formdata.append("username", email);
+    formdata.append("password", password);
 
-  var requestOptions = {
-    method: 'POST',
-    body: formdata,
-    redirect: 'follow'
-  };
+    var requestOptions = {
+        method: 'POST',
+        body: formdata,
+        redirect: 'follow'
+    };
 
-  fetch(`${api}/v1/auth/login`, requestOptions)
-    .then(response => response.json())
-    .then(result => {
-      user = {
-        token : result.msgServer.access_token,
-        name : result.msgServer.user.person.name,
-        id : result.msgServer.user.person.id,
-        avatar : result.msgServer.user.person.avatar,
-      };
-      $("#user-ku").html(user.name);
-      socket.emit("createUser", JSON.stringify({id : user.id, name : user.name})); //connect to web socket
-      listRoom();
-    })
-    .catch(error => console.log('error', error));
+    fetch(`${api}/v1/auth/login`, requestOptions)
+        .then(response => response.json())
+        .then(result => {
+        user = {
+            token : result.msgServer.access_token,
+            name : result.msgServer.user.person.name,
+            id : result.msgServer.user.person.id,
+            avatar : result.msgServer.user.person.avatar,
+        };
+        $("#user-ku").html(user.name);
+        socket.emit("createUser", JSON.stringify({id : user.id, name : user.name})); //connect to web socket
+        listRoom();
+        })
+        .catch(error => console.log('error', error));
 }
--- server.js
+++ server.js
@@ -1,111 +1,89 @@
 const express = require("express");
 const http = require("http");
-
 const app = express();
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server, {
-  cors: {
+const cors = {
     origin: "*",
     methods: "*",
     // allowedHeaders: ["my-custom-header"],
     // credentials: true
   }
-});
+const io = new Server(server, { cors: cors });
 const port = process.env.PORT || 5000
-
 app.use(express.static("public"));
 
 // Global variables to hold all users and rooms created
-var users = {};
-var rooms = [
-  { name: "global", creator: "Anonymous" },
-];
+let users = {};
+// let rooms = [ { name: "global", creator: "Anonymous" } ];
 
 io.on("connection", function (socket) {
-  console.log(`User connected to server.`);
+    socket.on("createUser", function (user) {
+        try {
+            user = JSON.parse(user);
+            user['socketId'] = socket.id;
+            socket.currentUser = user;
+            users[user.id] = user;
+            socket.currentRoom = "global";
+            socket.join("global");
+            console.log(`User ${user.name} created on server successfully.`);
+            socket.emit("updateChat", "INFO", JSON.stringify({ message : "You have joined global room" }));
+            socket.broadcast.to("global").emit("updateChat", 'INFO' , JSON.stringify({ message : `${user.name} has joined global room` }));
+            io.sockets.emit("updateUsers", users);
+            // socket.emit("updateRooms", rooms, "global");
+        }catch (error) {
+            console.log(error);
+            socket.emit("onError", "INFO", JSON.stringify({ message : error.message }));
+        }
+    });
 
-  socket.on("createUser", function (user) {
-    user = JSON.parse(user);
-    user['socketId'] = socket.id;
-    socket.currentUser = user;
-    users[user.id] = user;
-    socket.currentRoom = "global";
-    socket.join("global");
-
-    console.log(`User ${user.name} created on server successfully.`);
-
-    socket.emit("updateChat", "INFO", "You have joined global room");
-    socket.broadcast
-      .to("global")
-      .emit("updateChat", "INFO", user.name + " has joined global room");
-    io.sockets.emit("updateUsers", users);
-    socket.emit("updateRooms", rooms, "global");
-  });
-
-  socket.on("sendMessage", function (data) {
-    if (!socket.currentUser) {
-      console.log(`User not detected disconnected from server.`);
-      return false;
-    };
-
-    dataDecode = JSON.parse(data);
-    io.sockets.to(socket.currentRoom).emit("updateChat", socket.currentUser, dataDecode.message);
-    targets = dataDecode.opposites;
-    try {
-        targets.forEach(element => {
-            target = users[element];
-            if (target != undefined) {
-                if (target.socketId != socket.id ){
-                    console.log(target.socketId + " target notif");
-                    io.to(target.socketId).emit("getNotif", "INFO", data);
+    socket.on("sendMessage", function (data) {
+        if (!socket.currentUser)return false;
+        try {
+            io.sockets.to(socket.currentRoom).emit("updateChat", "NEW", data);
+            dataDecode = JSON.parse(data);
+            targets = dataDecode.opposites;
+            targets.forEach(element => {
+                target = users[element];
+                if (target != undefined) {
+                    if (target.socketId != socket.id ){
+                        io.to(target.socketId).emit("getNotif", "INFO", data);
+                    }
                 }
-            }
-        });
-    } catch (error) {
-        console.log(error);
-    }
-  });
+            });
+        } catch (error) {
+            console.log(error);
+            socket.emit("onError", "INFO", JSON.stringify({ message : error.message }));
+        }
+    });
 
-  socket.on("updateRooms", function (room) {
-    if (!socket.currentUser) {
-      console.log(`User not detected disconnected from server.`);
-      return false;
-    };
-    socket.broadcast
-      .to(socket.currentRoom)
-      .emit("updateChat", "INFO", socket.currentUser.name + " left room");
-    socket.leave(socket.currentRoom);
-    socket.currentRoom = room.id;
-    socket.join(room.id);
-    socket.emit("updateChat", "INFO", "You have joined " + room.name + " room");
-    socket.broadcast
-      .to(room.id)
-      .emit(
-        "updateChat",
-        "INFO",
-        socket.currentUser.name + " has joined this room"
-      );
-  });
+    socket.on("updateRooms", function (room) {
+        if (!socket.currentUser) return false;
+        try {
+            socket.broadcast.to(socket.currentRoom).emit("updateChat", "INFO", JSON.stringify({ message : `${socket.currentUser.name} left room` }));
+            socket.leave(socket.currentRoom);
+            socket.currentRoom = room.id;
+            socket.join(room.id);
+            socket.emit("updateChat", "INFO", JSON.stringify({ message : `You have joined ${room.name} room` }));
+            socket.broadcast.to(room.id).emit("updateChat", "INFO", JSON.stringify({ message : `${socket.currentUser.name} has joined this room`}));
+        } catch (error) {
+            console.log(error);
+            socket.emit("onError", "INFO", JSON.stringify({ message : error.message }));
+        }
+    });
 
-  socket.on("disconnect", function () {
-    if (!socket.currentUser) {
-      console.log(`User not detected disconnected from server.`);
-      return false;
-    };
-
-    console.log(`User ${socket.currentUser.name} disconnected from server.`);
-    io.sockets.emit("updateUsers", users);
-    socket.broadcast.emit(
-      "updateChat",
-      "INFO",
-      socket.currentUser.name + " has disconnected"
-    );
-
-    delete users[socket.currentUser.id];
-  });
+    socket.on("disconnect", function () {
+        if (!socket.currentUser) return false;
+        try {
+            io.sockets.emit("updateUsers", users);
+            socket.broadcast.emit( "updateChat","INFO", JSON.stringify({ message : `${socket.currentUser.name} has disconnected` }));
+            delete users[socket.currentUser.id];
+            console.log(`User ${socket.currentUser.name} disconnected.`);
+        }catch (error) {
+            console.log(error);
+            socket.emit("onError", "INFO", JSON.stringify({ message : error.message }));
+        }
+    });
 });
 
-server.listen(port, function () {
-  console.log("Listening to port " + port);
-});
+server.listen(port, () => console.log("Listening to port " + port));
